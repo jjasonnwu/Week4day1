26159d3ab4d987d9d6285b16f5381ea5ad88e7a2
jjasonnwu <jjasonnwu@gmail.com>
2022-10-25 13:54:16 -0400

    add Wild Wild Life answered

--- index.js
+++ index.js
@@ -29,32 +29,54 @@
 // Fizz Buzz
 ////////////////////////////////
 
-const getMyNumbers = () =>{
-    let num;
-        for(let i=1; i<101; i++){
-            num=i;
+// const getMyNumbers = () =>{
+//     let num;
+//         for(let i=1; i<101; i++){
+//             num=i;
         
-        if(num%3===0 && num%5===0){
-            console.log(FizzBuzz);
-        }
-        else if(num%3===0){
-            console.log(Fizz);
-        }
-        else if(num%5===0){
-            console.log(Buzz);
-        }
-        else {
-            console.log(num);
-        }
-    }
-}
-getMyNumbers();
+//         if(num%3===0 && num%5===0){
+//             console.log(FizzBuzz);
+//         }
+//         else if(num%3===0){
+//             console.log(Fizz);
+//         }
+//         else if(num%5===0){
+//             console.log(Buzz);
+//         }
+//         else {
+//             console.log(num);
+//         }
+//     }
+// }
+// getMyNumbers();
 
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee","plant", 5000, "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+
+// 1
+plantee[2] += 1;
+console.log(plantee)
+
+// 2
+wolfy[3] = "Gotham City"
+
+//3 
+dart[3] += " Hawkins"
+
+console.log(dart)
+
+//4
+wolfy.shift();
+wolfy.unshift('Gameboy');
+console.log(wolfy);
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
