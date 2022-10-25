3bffc838da00e7b9ef6c1a9ba22fbbef732d767e
jjasonnwu <jjasonnwu@gmail.com>
2022-10-25 13:42:04 -0400

    add getNumbers function

--- index.js
+++ index.js
@@ -16,18 +16,39 @@
 // Get Even
 ////////////////////////////////
 
-const getEven = () => {
-    for(let i = 0; i <= 200; i++){
-        if(i%2 ===0 && i !==0){
-            console.log(i);
-        }
-    }
-}
-getEven();
+// const getEven = () => {
+//     for(let i = 0; i <= 200; i++){
+//         if(i%2 ===0 && i !==0){
+//             console.log(i);
+//         }
+//     }
+// }
+// getEven();
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+
+const getMyNumbers = () =>{
+    let num;
+        for(let i=1; i<101; i++){
+            num=i;
+        
+        if(num%3===0 && num%5===0){
+            console.log(FizzBuzz);
+        }
+        else if(num%3===0){
+            console.log(Fizz);
+        }
+        else if(num%5===0){
+            console.log(Buzz);
+        }
+        else {
+            console.log(num);
+        }
+    }
+}
+getMyNumbers();
 
 
 ////////////////////////////////
